dc5e56cba1e1d8a8fd850cf946eb23da7e5f61b5
X-Mu-UAT <ibnhill@gmail.com>
2026-05-22 14:50:31 -0400

    Update ShipMovement.cs

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -2,14 +2,30 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+//These are the variables for the movement. they always go at the very top
+[Header(”Movement Speeds”)]
+[Tooltip(”Units per second for local movement.”)]
+public float localspeed = 5f;
+[Tooltip(”Degrees per second for local rotation.”)]
+public float rotationSpeed = 180f;
+[Tooltip(”Units per second for world movement.”)]
+public float worldSpeed = 5f
 
-    // Update is called once per frame
-    void Update()
+//Custom methods go below the variables
+// There is no void start() or void Update() for this
+
+//Action 1: Handles WASD moving and sliding
+public void MoveLocal(float verticalInput)
+    {
+        transform.Translate(Vector3.up * verticalInput * localSpeed* Time.deltaTime, Space.Self);
+    }
+//Action 2: Handles Turing left/right
+public void RotateLocal(float horizontalInput)
+{
+transform.Rotate(Vector3.forward * -horizontalInput * rotationSpeed * Time.deltaTime);
+}
+//Action 3: Handles Arrow keys moving globally
+public void MoveWorld(Vector2 direction)
     {
         
     }
