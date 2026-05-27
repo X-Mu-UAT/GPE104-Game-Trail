13043e779331f31d14df83cb63a22ec6cc5374bf
X-Mu-UAT <ibnhill@gmail.com>
2026-05-27 13:28:56 -0400

    Update DamageOnCollision.cs
    Collision script

--- Assets/Scripts/DamageOnCollision.cs
+++ Assets/Scripts/DamageOnCollision.cs
@@ -3,14 +3,24 @@ using UnityEngine;
 public class DamageOnCollision : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int damageAmount = 25;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        DealDamage(other.gameObject);
+    }
+    {
+        DealDamage(collision.gameObject);
+    }
+    private void DealDamage(GameObject objectHit)
+    {
+        Health targetHealth = objectHit.GetComponent<Health>();
+
+        if (targetHealth !=null
+
+        {
+            targetHealth.TakeDamage(damageAmount);
+            Debug.Log(gameObject.name + “dealt damage to “ + objectHir.name);
+        }
     }
 }
