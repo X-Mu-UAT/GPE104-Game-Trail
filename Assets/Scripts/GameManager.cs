cb641622c49e38741c103d0bf6afee168d423045
X-Mu-UAT <ibnhill@gmail.com>
2026-05-29 13:49:36 -0400

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // tThe static reference that makes it a singleton
+    public static GameManager Instance {get; private set;}
+
+    [Header(”Game State”)]
+    public List<Health> obstacles = new List<Health>();
+    peivate bool isGameOver = false
+
+void Awake()
     {
-        
+        if (Instance == null)
+    {
+            Instance = this;
+            DontDestroyOnLoad(GameObject);
+        }
+        else
+        {
+            Destroy(GameObject); // To get rid of duplicates
+        }
+    }
+    public void RegisterObstacle(Health obstacle)
+  {
+        if (!obstacles.Contains(obstacle))
+        {
+            obstacles.Add(obstacle);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called when an obstacle dies
+    public void UnregisterObstacle(Health obstacle)
     {
-        
+        if (obstacles.Contains(obstacle))
+        {
+            obstacles.Remove(obstacle);
+        }
+
+        // Check for Victory condition
+        if (obstacles.Count == 0 && !isGameOver)
+        {
+            Victory();
+        }
+    }
+
+    public void TargetDestroyed(bool isPlayer)
+    {
+        if (isGameOver) return;
+
+        if (isPlayer)
+        {
+            isGameOver = true;
+            Debug.Log("Failure");
+        }
+    }
+
+    private void Victory()
+    {
+        isGameOver = true;
+        Debug.Log("Victory");
     }
 }
