c791684ed916e0b2bd7c9bf81634b05f9f0b3d6c
X-Mu-UAT <ibnhill@gmail.com>
2026-05-29 13:53:50 -0400

    Update Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Projectile Settings")]
+    public float speed = 10f;
+    public float damageAmount = 10f; // Designer-adjustable in inspector
+    public float lifeSpan = 3f;
+
     void Start()
     {
-        
+        Destroy(gameObject, lifeSpan); // Clean up bullet if it hits nothing
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move forward constantly in its local facing direction
+        transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
+    }
+
+    // Trigger collision detection
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Health targetHealth = other.GetComponent<Health>();
+        if (targetHealth != null)
+        {
+            targetHealth.TakeDamage(damageAmount);
+            Destroy(gameObject); // Destroy the bullet upon impact
+        }
     }
 }
