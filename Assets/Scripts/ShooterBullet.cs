99bed797a17536fbf7cfea1ef2264daadb69af16
X-Mu-UAT <ibnhill@gmail.com>
2026-05-29 13:51:22 -0400

    Update ShooterBullet.cs

--- Assets/Scripts/ShooterBullet.cs
+++ Assets/Scripts/ShooterBullet.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class ShooterBullet : MonoBehaviour
+public class ShooterBullet : Shooter
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Override the base class method (Polymorphism)
+    public override void Shoot()
     {
-        
-    }
+        if (projectilePrefab == null) return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Calculate position 1 unit in front of the ship (using 2D transform.up based on your previous rotation script)
+        Vector3 spawnPosition = transform.position + (transform.up * 1f);
+
+        // Spawn the projectile
+        GameObject bullet = Instantiate(projectilePrefab, spawnPosition, transform.rotation);
     }
 }
